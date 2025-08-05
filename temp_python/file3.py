fc7d0abfc3ae86faeab59b9c101b76b7118e3f2e
Brandon H <97060762+bshowell0@users.noreply.github.com>
2025-08-05 10:20:51 -0700

    Update file3.py

--- temp_python/file3.py
+++ temp_python/file3.py
@@ -3,8 +3,8 @@
 thing = "This doesn't need anything crazy"
 
 def abc():
-    return thing + " change 4"
+    return thing + " change 5"
 
-def lol2():
-    # deleted lol, added lol2, changed abc
+def lol3():
+    # deleted lol2, added lol3, changed abc
     return 1
